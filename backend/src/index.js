7f881b86a78a0840a3b1363c7e0b4f7cb0bc9ab0
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2023-12-01 09:25:12 -0300

    feat:serve running

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(8080, () => {
-    console.log("backend serve running (video 8) 10:58");
+    console.log("backend serve running (video 8) 52:41");
 });
